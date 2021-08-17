6d27f21ff959126ffccef32c51af0421ab64f2e2
dulecux <dusan.culibrk@live.com>
2021-08-18 00:24:53 +0200

    routing and fetch data

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus accusamus quidem deleniti harum impedit adipisci delectus incidunt dolor ullam soluta quae nihil magni qui quaerat esse perferendis id, expedita architecto doloribus recusandae iusto dolorem tempore distinctio! Quo eligendi eos possimus aliquid officia impedit obcaecati. Aliquam possimus libero nemo odio inventore sequi, repellendus dolorum dicta fugiat unde eaque atque nisi tenetur tempore quasi sed. Quos fugiat numquam dicta iste culpa quod odit, atque corrupti quo quae! Ea ad modi fugiat debitis id? Facilis blanditiis dolor modi aspernatur doloremque aliquam voluptatem suscipit! Nihil repellat ducimus voluptatem ex dignissimos excepturi autem id placeat!</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et pariatur deserunt atque enim, cupiditate quas magnam vel adipisci, libero harum ut aut quos nemo sint incidunt a repellendus nisi tempore facilis. Totam officiis aut nisi deleniti. Tempora odio earum similique dolor assumenda neque quas debitis facilis voluptate. Alias id obcaecati, maxime temporibus harum amet ea. Voluptates fugit rerum dolor id eos nulla velit repellat saepe molestias iusto! Hic iure dolorum, molestiae corrupti quos laborum maiores nihil adipisci, incidunt, sunt officiis voluptate a placeat. Tempora, omnis hic suscipit odit delectus illum, dolore iusto saepe quis, est quibusdam explicabo commodi ipsa animi?</p>
+    
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Homepage</h1>
+   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, delectus. Rerum saepe reiciendis quo nisi rem, exercitationem velit, voluptates repudiandae nesciunt est ea ducimus temporibus qui labore voluptatum! Quo deleniti odio consequatur blanditiis aliquid nulla at reiciendis expedita, dolor officiis, illo, ab similique. Est obcaecati facilis minus maxime, error assumenda soluta saepe, eum aperiam dolor perspiciatis. Suscipit, ab, odit dolorem aspernatur, maiores ut asperiores quam unde cum soluta dolores repellendus illo doloremque. Praesentium, explicabo unde eaque doloremque doloribus qui commodi dolore quisquam molestiae reiciendis. Minus ipsam porro expedita obcaecati ratione voluptatibus sunt, laudantium, mollitia repellat consequuntur quasi voluptatum, totam nisi.</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt delectus dolore distinctio ullam alias animi perferendis hic commodi eius, molestiae quae laudantium nostrum tempora reprehenderit, a quaerat nesciunt error consectetur saepe! Molestiae, sunt in ab exercitationem magni corrupti pariatur? Quas, unde! A qui aut eos in unde! Rem nisi quas sed eveniet molestias ipsam, quae minus, molestiae iste quo quam, omnis sit animi ex dolor eum distinctio. Atque voluptates quibusdam accusamus enim delectus! Error fugit accusantium nesciunt fugiat explicabo facere ad corrupti doloribus nostrum consequatur soluta amet, recusandae est magnam tempora. Rerum voluptatibus accusantium dolorem beatae laudantium facilis minus nam?</p>
+   
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  
 }
 </script>
